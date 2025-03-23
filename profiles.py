c46013bd7260a938b8bed125d9d45ecfdf2f2f8a
Parth Parikh <parthparikh1999p@gmail.com>
2025-03-23 16:51:32 -0700

    Pong simulation with normal distribution

--- profiles.py
+++ profiles.py
@@ -49,25 +49,18 @@ class GameStats:
                 if random.random() < p1_win_probability:
                     winner, loser = player1, player2
                     k_winner = max(1, 32 - 0.04 * (p1_rank - 2000))
-                    k_loser = max(1, 32 - 0.04 * (p2_rank - 2000))
+                    k_loser = max(32, 32 - 0.04 * (p2_rank - 2000))
                     self.player_elo[winner] += k_winner * (1 - p1_win_probability)
-                    self.player_elo[loser] += k_loser * (1 - (1 - p1_win_probability))
+                    self.player_elo[loser] += k_loser * (0 - p2_win_probability)
                 else:
                     winner, loser = player2, player1
-                    k_winner = max(1, 32 - 0.04 * (p1_rank - 2000))
-                    k_loser = max(1, 32 - 0.04 * (p2_rank - 2000))
+                    k_winner = max(1, 32 - 0.04 * (p2_rank - 2000))
+                    k_loser = max(32, 32 - 0.04 * (p1_rank - 2000))
                     self.player_elo[winner] += k_winner * (1 - p2_win_probability)
-                    self.player_elo[loser] += k_loser * (1 - (1 - p2_win_probability))
+                    self.player_elo[loser] += k_loser * (0 - p1_win_probability)
 
                 winner_points = 21
                 loser_points = random.randint(0, winner_points - 1)
-
-                fastest_speed = random.randint(100, 250)
-                avg_speed = random.randint(80, fastest_speed)
-                aces = random.randint(0, 3)
-                rallies = random.randint(10, 30)
-                longest_rally = random.randint(5, rallies)
-                avg_rally = round(rallies / random.uniform(1.0, 3.0), 1)
 
                 self.game_stats.append(
                     {
@@ -79,12 +72,6 @@ class GameStats:
                         "result": "W",
                         "points_scored": winner_points,
                         "points_allowed": loser_points,
-                        "fastest_ball_speed": fastest_speed,
-                        "average_ball_speed": avg_speed,
-                        "aces": aces,
-                        "rallies": rallies,
-                        "longest_rally": longest_rally,
-                        "average_rally": avg_rally,
                     },
                 )
 
@@ -98,12 +85,6 @@ class GameStats:
                         "result": "L",
                         "points_scored": loser_points,
                         "points_allowed": winner_points,
-                        "fastest_ball_speed": fastest_speed,
-                        "average_ball_speed": avg_speed,
-                        "aces": random.randint(0, aces),
-                        "rallies": rallies,
-                        "longest_rally": random.randint(0, longest_rally),
-                        "average_rally": avg_rally,
                     },
                 )
 
@@ -115,8 +96,8 @@ class GameStats:
         print("Tournament complete! Winner is:", round_players[0])
 
     def assign_init_elo(self, player_ids, player_rankings):
-        upper_bound = 2500
-        lower_bound = 2100
+        upper_bound = 2700
+        lower_bound = 2400
         delta = (upper_bound - lower_bound) / len(player_ids)
         for player_id in player_ids:
             self.player_elo[player_id] = upper_bound - (
@@ -133,11 +114,16 @@ if __name__ == "__main__":
 
     game_stats_obj.assign_init_elo(player_ids, player_rankings)
 
-    for tournament_id in range(1, 100):
+    for tournament_id in range(1, 15):
         game_stats_obj.simulate_tournament(player_ids, tournament_id)
 
     sorted_players = sorted(
         game_stats_obj.player_elo.items(), key=lambda x: x[1], reverse=True
     )
-    print("Top 10 players after tournament", tournament_id)
-    print(sorted_players[:10])
+    player_elo_df = pd.DataFrame(sorted_players, columns=["Player ID", "ELO Rating"])
+    print(player_elo_df.head(10))
+    print(player_elo_df.tail(10))
+    print(
+        "Delta is: ",
+        player_elo_df["ELO Rating"].max() - player_elo_df["ELO Rating"].min(),
+    )
